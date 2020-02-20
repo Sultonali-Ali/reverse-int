8159f3409fe2d73b5cc4d53043c5fd97e79e8839
Jalolov Sultonali <jsultonali1993@mail.ru>
2020-02-20 09:37:32 +0500

    feat: finish task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
-}
+  let nString = '' + n;
+  if (nString[0] === '-') {
+      nString = nString.slice(1);
+  }
+  return +nString.split('').reverse().join('');
+};
